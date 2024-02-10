5f938c0fcb7b8280bac4b67b4d3eb1319c9a92ef
Ashish Yadav <ashishyadav@Ashishs-MacBook-Air.local>
2024-02-10 21:11:17 +0530

    uplaod image on s3 bucket successfully

--- server/routes/image.js
+++ server/routes/image.js
@@ -3,10 +3,9 @@ const router = express.Router();
 const { v4: uuidv4 } = require('uuid');
 const { pool } = require('../config');
 const multer = require('multer');
+const { uploadFile } = require('../s3')
 
-const imageUpload = multer({
-    dest: 'images',
-});
+
 
 
 const fileFilter = (req, file, cb) => {
@@ -17,34 +16,80 @@ const fileFilter = (req, file, cb) => {
     }
 };
 
-const upload = multer({ storage: storage, fileFilter: fileFilter });
+const imageUpload = multer({
+    dest: 'images',
+    fileFilter: fileFilter
+});
+
+const upload = multer({ storage: '/dist', });
 
 // Image Upload Routes
-router.post('/', upload.single('file'), (req, res) => {
-    console.log("req----<", req.file)
+// router.post('/', imageUpload.single('file'), async (req, res) => {
+//     console.log("req----<", req.file)
+//     const { filename, mimetype, size } = req.file;
+//     console.log("filename --->", filename, mimetype, size)
+//     const filepath = req.file.path;
+//     console.log("filepath --->", filepath)
+
+//     const insertQuery = 'INSERT INTO images (id, filename, filepath, mimetype, size) VALUES ($1, $2, $3, $4, $5) RETURNING *';
+//     const values = [uuidv4(), filename, filepath, mimetype, size];
+
+
+//     const s3Result = await uploadFile(req.file)
+//     console.log("s3Result -=--", s3Result)
+
+
+//     // pool.query(insertQuery, values, async (error, result) => {
+//     //     if (error) {
+//     //         console.error('Error inserting image:', error);
+//     //         res.json({ success: false, message: 'upload failed', stack: error.stack });
+//     //     } else {
+//     //         const insertedImage = result.rows[0];
+//     //         console.log('Image inserted successfully:', insertedImage);
+//     //         const s3Result = await uploadFile(req.file)
+//     //         console.log("s3Result -=--", s3Result)
+//     //         res.status(201).json({
+//     //             success: true,
+//     //             filename
+//     //         });
+
+//     //     }
+//     // })
+// });
+
+
+router.post('/', imageUpload.single('file'), async (req, res) => {
+    const file = req.file
+    console.log("req.file ---->", req.file)
+    console.log("file --->", file)
+
     const { filename, mimetype, size } = req.file;
-    console.log("filename --->", filename, mimetype, size)
-    const filepath = req.file.path;
-    console.log("filepath --->", filepath)
+
+    const result = await uploadFile(file)
+    // await unlinkFile(file.path)
+    console.log("result ---->", result)
 
     const insertQuery = 'INSERT INTO images (id, filename, filepath, mimetype, size) VALUES ($1, $2, $3, $4, $5) RETURNING *';
-    const values = [uuidv4(), filename, filepath, mimetype, size];
+    const values = [uuidv4(), result?.Key, file.path, mimetype, size];
 
-
-    pool.query(insertQuery, values, (error, result) => {
+    pool.query(insertQuery, values, async (error, result) => {
         if (error) {
             console.error('Error inserting image:', error);
             res.json({ success: false, message: 'upload failed', stack: error.stack });
         } else {
             const insertedImage = result.rows[0];
             console.log('Image inserted successfully:', insertedImage);
-            res.status(201).json({
-                success: true,
-                filename
-            });
+            // const s3Result = await uploadFile(req.file)
+            // console.log("s3Result -=--", s3Result)
+            // res.status(201).json({
+            //     success: true,
+            //     filename
+            // });
 
         }
     })
-});
+    const description = req.body.description
+    res.send({ imagePath: `/images/${result.Key}` })
+})
 
 module.exports = router;
